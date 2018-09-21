8c8b48960902e75e3283a11bf1682cba5ea36c56
Julian Andres Maldonado Aponte <Jmaldonadoa@intergrupo.com>
2018-09-21 18:50:31 -0500

    Se utiliza sensor heart rate.

--- HelpIG/HelpIG/MainActivity.cs
+++ HelpIG/HelpIG/MainActivity.cs
@@ -12,13 +12,37 @@ using Android.Support.V4.View;
 using Android.Support.Wearable.Activity;
 using Java.Interop;
 using Android.Views.Animations;
+using Android.Hardware;
 
 namespace HelpIG
 {
     [Activity(Label = "@string/app_name", MainLauncher = true)]
-    public class MainActivity : WearableActivity
+    public class MainActivity : WearableActivity, ISensorEventListener
     {
         private TextView _textView;
+        private SensorManager mSensorManager;
+        private float currentValue = 0f;
+
+        public void OnAccuracyChanged(Sensor sensor, [GeneratedEnum] SensorStatus accuracy)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnSensorChanged(SensorEvent e)
+        {
+            if (e.Sensor.Type == SensorType.HeartRate && e.Values.Count > 0)    
+            {
+                float newValue = (float)Math.Round(e.Values[0]);
+                //Log.d(LOG_TAG,sensorEvent.sensor.getName() + " changed to: " + newValue);
+                // only do something if the value differs from the value before and the value is not 0.
+                if (currentValue != newValue && newValue != 0)
+                {
+                    // save the new value
+                    currentValue = newValue;
+                    _textView.Text = currentValue.ToString();
+                }
+            }
+        }
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -26,7 +50,25 @@ namespace HelpIG
             SetContentView(Resource.Layout.activity_main);
 
             _textView = FindViewById<TextView>(Resource.Id.text);
-            SetAmbientEnabled();
+            //SetAmbientEnabled();
+
+            mSensorManager = (SensorManager)GetSystemService(Context.SensorService);
+            Sensor mHeartRate = mSensorManager.GetDefaultSensor(SensorType.HeartRate);
+            mSensorManager.RegisterListener(this, mHeartRate, SensorDelay.Fastest);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            // Listen sensor manger on resume activity of app
+            mSensorManager.RegisterListener(this, mSensorManager.GetDefaultSensor(SensorType.HeartRate), SensorDelay.Fastest);
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            // stop listening if app is no longer used. This is importent step otherwise it will drain your watch battery.
+            mSensorManager.UnregisterListener(this);
         }
     }
 }
